c1f1129a28dbd78da5b9917f53e6988c06b4de4e
Oluro Olaoluwa <ollaollu@gmail.com>
2015-08-24 17:42:33 +0000

    Added ohm gem, created Carts controller and dev environment switch from Windows

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823181821) do
+ActiveRecord::Schema.define(version: 20150824164825) do
 
   create_table "pictures", force: :cascade do |t|
     t.datetime "created_at",         null: false
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 20150823181821) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "phone"
+    t.string   "address"
+    t.string   "country"
+    t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
